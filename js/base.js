1f32712393c448b90ca000e78a44b6255c80cace
Melissa M. Dailey <mel.dailey@gmail.com>
2015-11-08 20:10:36 -0800

    final styling

--- js/base.js
+++ js/base.js
@@ -1,9 +1,6 @@
 
 var monthly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-
-
 var map;
-
 
 $(document).ready(function(){
  
@@ -15,9 +12,12 @@ $(document).ready(function(){
 	  });
 
 
+	
+	// get the data
 	$.get(monthly_quakes_endpoint, function(data) {
 		var features = data.features;
 
+		// loop through the data
 		features.forEach(function(element) {
 			//Show me the title
 			var title = element.properties.title;
@@ -38,48 +38,35 @@ $(document).ready(function(){
 			var location = loc[1];
 
 			// append data to html
-			$("#info").append("<p><span>" + mag + "</span>" + " quake on " + date , "<p class='loc'>" + location + "</p><hr>");
+			$("#info").append("<p><span class='mag'>" + mag + "</span>" + "<span class='quake-info'>" + "quake on " + date + "</span></p>" +"<p><span class='loc'> in" + location + "</span></p><hr>");
  		
 			//Show me the coordinates
  			var narrowGeo = element.geometry;
  			var narrowCoo = narrowGeo["coordinates"];
  			var coordinates = {lat: narrowCoo[1], lng: narrowCoo[0]};
+
+
+ 			var contentString = '<h3>'+ location + ", magnitude " + mag+'</h3>';
+
+ 			var infowindow = new google.maps.InfoWindow({
+ 			  content: contentString
+ 			});
  		 			
 			// assign markers to coordinates
 			var marker = new google.maps.Marker({
 			  position: coordinates,
 			  map: map,
-			  title: title
+			  title: title, 
+			  animation: google.maps.Animation.DROP
 			}); 
+
+			marker.addListener('click', function() {
+			  infowindow.open(map, marker);
+			});
+
 			//render the marker
 			marker.setMap(map);
 		});
 	});
 });
-
-
-/*
-
-
-
-
-
-
-Pseudo code: 
--When the document is ready 
-	-render the map
-		-request the data
-			- when the data is back 
-				-render the info
-				-render the map markers
-
--when the map is ready
-	-bind the click listener
-		-when the respond element is clicked
-			-respond!
-
-
-			*/
-
-
 
